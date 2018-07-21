c1d97bc523345d73b0966a3d9781e0b4aa72d4f6
Rob Jinman <jinmanr@gmail.com>
2018-07-21 20:50:30 +0100

    Increased difficulty of making_progress

--- src/fragments/f_main/f_app_dialog/f_procalc_setup/game_logic.cpp
+++ src/fragments/f_main/f_app_dialog/f_procalc_setup/game_logic.cpp
@@ -146,8 +146,8 @@ void GameLogic::setElevatorSpeed() {
   CElevatorBehaviour& elevator =
     m_entityManager.getComponent<CElevatorBehaviour>(elevatorId, ComponentKind::C_BEHAVIOUR);
 
-  const double MIN_SPEED = 6;
-  const double MAX_SPEED = 30;
+  const double MIN_SPEED = 4;
+  const double MAX_SPEED = 22;
   const double MAX_FEATURES = 17;
   double delta = (MAX_SPEED - MIN_SPEED) / MAX_FEATURES;
 
