26e7e3b3ec7da3c1427e550591ac325b573bc820
Rob Jinman <jinmanr@gmail.com>
2018-07-23 00:08:35 +0100

    Refactored release script into functions

--- release/release.sh
+++ release/release.sh
@@ -32,55 +32,68 @@ if [ ! "${branch::1}" == "*" ]; then
   exit 1
 fi
 
-git add ./VERSION
-git commit -m "Updated VERSION"
+commit_version() {
+  git add ./VERSION
+  git commit -m "Updated VERSION"
+}
 
-if [ $snapshot == true ]; then
-  echo "Building snapshot release"
-  ./release/build_deb.sh -rsx -k $gpg_key
-else
-  echo "Building stable release"
-  ./release/build_deb.sh -rs -k $gpg_key
-fi
+build_deb_source_package() {
+  if [ $snapshot == true ]; then
+    echo "Building snapshot release"
+    ./release/build_deb.sh -rsx -k $gpg_key
+  else
+    echo "Building stable release"
+    ./release/build_deb.sh -rs -k $gpg_key
+  fi
 
-changes_file="$(find .. -name procalc*.changes)"
+  changes_file="$(find .. -name procalc*.changes)"
 
-if [ $snapshot == true ]; then
-  dput ppa:rjinman/snapshots "$changes_file"
-fi
-dput ppa:rjinman/ppa "$changes_file"
+  if [ $snapshot == true ]; then
+    dput ppa:rjinman/snapshots "$changes_file"
+  fi
+  dput ppa:rjinman/ppa "$changes_file"
 
-git add ./debian/changelog
-git commit -m "Updated debian/changelog"
+  git add ./debian/changelog
+  git commit -m "Updated debian/changelog"
 
-git push
+  git push
+}
 
-git checkout master
-git merge develop
+tag_master() {
+  git checkout master
+  git merge develop
 
-if [ $snapshot != true ]; then
-  git tag -a "v$version" -m "Pro Office Calculator v$version"
-fi
+  if [ $snapshot != true ]; then
+    git tag -a "v$version" -m "Pro Office Calculator v$version"
+  fi
 
-git push --follow-tags
-git checkout develop
+  git push --follow-tags
+  git checkout develop
+}
 
-win_artifact="$(find ./artifacts -name *.msi)"
-osx_artifact="$(find ./artifacts -name *.app.zip)"
+upload_artifacts() {
+  win_artifact="$(find ./artifacts -name *.msi)"
+  osx_artifact="$(find ./artifacts -name *.app.zip)"
 
-full_version="$(get_tarball_version ..)"
+  full_version="$(get_tarball_version ..)"
 
-win_artifact_name="ProOfficeCalculator_${full_version}.msi"
-osx_artifact_name="ProOfficeCalculator_${full_version}.app.zip"
+  win_artifact_name="ProOfficeCalculator_${full_version}.msi"
+  osx_artifact_name="ProOfficeCalculator_${full_version}.app.zip"
 
-mv "$win_artifact" "./artifacts/$win_artifact_name"
-mv "$osx_artifact" "./artifacts/$osx_artifact_name"
+  mv "$win_artifact" "./artifacts/$win_artifact_name"
+  mv "$osx_artifact" "./artifacts/$osx_artifact_name"
 
-cat > ./artifacts/manifest << EOL
+  cat > ./artifacts/manifest <<EOF
 <artifacts>
   <windows>${win_artifact_name}</windows>
   <osx>${osx_artifact_name}</osx>
 </artifacts>
-EOL
+EOF
 
-aws s3 cp ./artifacts/ s3://proofficecalculator.com/downloads/ --recursive
+  aws s3 cp ./artifacts/ s3://proofficecalculator.com/downloads/ --recursive
+}
+
+commit_version
+build_deb_source_package
+tag_master
+upload_artifacts
